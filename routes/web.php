c4039169585adfea895ed5b6f6d3386d1cce80af
ShubhamDev021 <shubhat021@gmail.com>
2025-03-13 10:58:00 +0530

    Adding constants to Routes: single constant

--- routes/web.php
+++ routes/web.php
@@ -21,14 +21,26 @@ Route::get('/greeting', function () {
     return "Hello World";
 });
 
+// Parameterized route: required parameter
 Route::get('/greeting/{id}', function ($id) {
     return "User ID is " . $id;
 });
 
+// Parameterized route: optional parameter having default null value
 Route::get('/greeting2/{name?}', function ($name = null) {
     return "User name is " . $name;
 });
 
+// Parameterized route: optional parameter having default string value
 Route::get('/greeting3/{name?}', function ($name = "Shubham") {
     return "User name is " . $name;
 });
+
+// Adding constants to Routes: single constant
+Route::get('user/{id}', function ($id) {
+    return "User ID: " . $id;
+})->where('id', '[0-9]+');
+
+Route::get('user/{name}', function ($name) {
+    return "User Name: " . $name;
+})->where('id', '[A-Za-z]+');
