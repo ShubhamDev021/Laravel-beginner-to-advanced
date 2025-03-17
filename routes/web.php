550366af1554ccde9472cf72352b282a66e01933
ShubhamDev021 <shubhat021@gmail.com>
2025-03-17 17:24:29 +0530

    Passing data in view - method 3

--- routes/web.php
+++ routes/web.php
@@ -72,7 +72,7 @@ Route::fallback(function () {
 // Return view using view function
 Route::get('/greeting4', function() {
     $name = "Shubham";
-    return view('greeting', compact('name'));
+    return view('greeting')->with('name', $name);
 });
 
 // Return view using View Facades
