7b8245aa15cb6f2466949959b628f378eacb0864
ShubhamDev021 <shubhat021@gmail.com>
2025-03-18 20:20:59 +0530

    Reverting last change

--- database/migrations/2025_03_18_141811_create_posts_table.php
+++ database/migrations/2025_03_18_141811_create_posts_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('description');
-            $table->boolean('is_published')->default(false);
             $table->timestamps();
         });
     }
