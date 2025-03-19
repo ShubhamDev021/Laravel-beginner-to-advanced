ceee42f59bd3b5b130ee938657e2a4a13c4cea24
ShubhamDev021 <shubhat021@gmail.com>
2025-03-19 16:39:40 +0530

    CRUD: Create a form to enter data

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Create Posts</title>
+    {{-- bootstrap CSS CDN --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    
+    <div class="row">
+        <div class="col-md-6 offset-3" style="margin-top: 40px;">
+            <h1 class="text-center">Create Post</h1>
+            <form>
+                <div class="mb-3">
+                    <label for="title" class="form-label">Title</label>
+                    <input type="text" class="form-control" id="title">
+                </div>
+                <div class="mb-3">
+                    <label for="description" class="form-label">Description</label>
+                    <textarea class="form-control" id="description"></textarea>
+                </div>
+                <div class="mb-3">
+                    <label for="is_active" class="form-label">Active</label>
+                    <select class="form-control" name="is_active" id="is_active">
+                        <option selected disabled>----Choose option----</option>
+                        <option value="1">Yes</option>
+                        <option value="0">No</option>
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <label for="is_published" class="form-label">Published</label>
+                    <select class="form-control" name="is_published" id="is_published">
+                        <option selected disabled>----Choose option----</option>
+                        <option value="1">Yes</option>
+                        <option value="0">No</option>
+                    </select>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
+    
+    {{-- bootstrap JS CDN --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
