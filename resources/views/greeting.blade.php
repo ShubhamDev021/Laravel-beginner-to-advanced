4438567e92092b92b0a9266c3bd3e99bd0da0d2a
ShubhamDev021 <shubhat021@gmail.com>
2025-03-19 15:54:06 +0530

    Use of route()

--- resources/views/greeting.blade.php
+++ resources/views/greeting.blade.php
@@ -10,5 +10,7 @@
     <h1>You are {{ $age }} years old</h1>
 
     <button><a href={{ url('greeting3') }}>Click here for url()</a></button>
+    <br>
+    <button><a href={{ route('greet') }}>Click here for route()</a></button>
 </body>
 </html>
