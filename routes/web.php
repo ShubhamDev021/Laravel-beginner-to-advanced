7065d01ed330ace16133343666d85941ce7cdd41
ShubhamDev021 <shubhat021@gmail.com>
2025-03-25 20:27:46 +0530

    Query Builder: get data by multiple conditions - AND operattion - method 2 - single where() function

--- routes/web.php
+++ routes/web.php
@@ -234,9 +234,17 @@ Route::get('query-builder-find-post-by-specific-condition/{value}', function ($v
 
 Route::get('query-builder-find-post-by-specific-conditions-AND/{value1}/{value2}', function ($value1, $value2) {
     //where() chaining method
+    // $post = DB::table('posts')
+    //         ->where('title', $value1)
+    //         ->where('is_active', $value2)
+    //         ->get();
+
+    //single where() method
     $post = DB::table('posts')
-            ->where('title', $value1)
-            ->where('is_active', $value2)
+            ->where([
+                'title'     =>  $value1,
+                'is_active' =>  $value2
+            ])
             ->get();
 
     return $post;
